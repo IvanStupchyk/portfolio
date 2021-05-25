7821355e434cd0be2e7a48ee386a1aca3583f9b4
IvanStupchuk <hardtrynew@rambler.ru>
2021-05-25 22:18:23 +0300

    update svg hovers

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect} from "react"
 import {NavLink, Route} from 'react-router-dom';
 import s from './content.module.scss'
 import {Home} from "./Home/Home";
@@ -8,8 +8,13 @@ import {Portfolio} from "./Portfolio/Portfolio";
 import {Contact} from "./Contact/Contact";
 
 export const Content = () => {
+    let a = useEffect(() => {
+        <Home />
+    })
+
     return (
         <div className={s.contentContainer}>
+            {a}
             <Route path={"/home"} render={() => <Home />}/>
             <Route path={"/about"} render={() => <About />}/>
             <Route path={"/port"} render={() => <Portfolio />}/>
